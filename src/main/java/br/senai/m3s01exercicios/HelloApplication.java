803201627d97adce7bd1999e829fb5b8d657c386
Rafael Albino <fael10pg@gmail.com>
2022-05-27 13:26:13 -0300

    Projeto criado

---
+++ src/main/java/br/senai/m3s01exercicios/HelloApplication.java
@@ -0,0 +1,9 @@
+package br.senai.m3s01exercicios;
+
+import javax.ws.rs.ApplicationPath;
+import javax.ws.rs.core.Application;
+
+@ApplicationPath("/api")
+public class HelloApplication extends Application {
+
+}
